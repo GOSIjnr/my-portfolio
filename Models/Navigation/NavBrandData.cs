709e7be7c4abe58f3c40c23ed64fb29d85dd0dd5
GOSIjnr <71373171+GOSIjnr@users.noreply.github.com>
2025-05-04 16:36:01 +0100

    Made the Navigation Logo Modular
    - imported google fonts
    - made modular components
    - imported new images

---
+++ Models/Navigation/NavBrandData.cs
@@ -0,0 +1,8 @@
+namespace MyPortfolio.Models.Navigation;
+
+public class NavBrandData
+{
+	public string LogoPath { get; set; } = null!;
+	public string ProjectName { get; set; } = null!;
+	public string AltText { get; set; } = "Project Logo";
+}
--- Models/Navigation/NavLinkItem.cs
+++ Models/Navigation/NavLinkItem.cs
@@ -1,10 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace MyPortfolio.Models.Navigation;
 
 public class NavLinkItem
 {
-	[Required] public required string Href { get; set; }
-	[Required] public required string Text { get; set; }
-	[Required] public required string CssClass { get; set; }
+	public string Href { get; set; } = "";
+	public string Text { get; set; } = "";
+	public string CssClass { get; set; } = "";
 }

8321990e24eaae0d539443e3f5de3ea91cc70aa0
GOSIjnr <71373171+GOSIjnr@users.noreply.github.com>
2025-05-24 05:37:18 +0100

    Update Form to Blazor EditForm

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+public class ContactFormModel
+{
+	public string FirstName { get; set; } = string.Empty;
+	public string LastName { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+	public string Phone { get; set; } = string.Empty;
+	public string Service { get; set; } = string.Empty;
+	public string Message { get; set; } = string.Empty;
+}

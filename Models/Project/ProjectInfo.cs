01d9d4c3c6348497ab6dc2f3f827f89a8b379584
GOSIjnr <71373171+GOSIjnr@users.noreply.github.com>
2025-05-30 11:34:39 +0100

    Made a Rough Prototype
    - made a rough prototype of how i want the project page to function
    - made a rough layout

--- Models/Project/ProjectInfo.cs
+++ Models/Project/ProjectInfo.cs
@@ -47,5 +47,5 @@ public class ProjectInfo(string title, string description, List<string> technolo
 		set => _otherLinks = value ?? [];
 	}
 
-	public ProjectInfo() : this("Lorem ispum", "dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", ["Lorem ispum"], "images/icon.svg", null, []) { }
+	public ProjectInfo() : this("Lorem ispum", "dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", ["Lorem ispum"], "images/icon.svg", null, []) { }
 }

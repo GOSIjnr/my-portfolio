06246d40e6477742cf70edece565935f78c18665
GOSIjnr <71373171+GOSIjnr@users.noreply.github.com>
2025-06-03 12:19:09 +0100

    Cleaned Up Project Card

--- Helpers/Utilities/ImageLoaderOptions.cs
+++ Helpers/Utilities/ImageLoaderOptions.cs
@@ -3,7 +3,7 @@ namespace MyPortfolio.Helpers.Utilities;
 public class ImageLoaderOptions
 {
 	public required string ElementId { get; set; }
-	public required string CssProperty { get; set; }
+	public string? CssProperty { get; set; }
 	public required string ImageUrl { get; set; }
 	public required string FallbackUrl { get; set; }
 	public bool UseVariable { get; set; } = false;

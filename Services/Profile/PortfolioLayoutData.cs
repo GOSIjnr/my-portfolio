483c7e460e21bf4cee73e1027c5f169319c935ce
GOSIjnr <71373171+GOSIjnr@users.noreply.github.com>
2025-06-06 15:59:27 +0100

    Cleaned up the Resume Component
    - made the resume component handle nulls gracefully
    - cleaned up some part of the codebase that was missed

--- Services/Profile/PortfolioLayoutData.cs
+++ Services/Profile/PortfolioLayoutData.cs
@@ -12,11 +12,11 @@ public class PortfolioLayoutData : IAppLayoutData
 	private static readonly List<NavigationItem> _navigationLinks =
 	[
 		new("Home", string.Empty, NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
-			new("Services", "services", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
-			new("Resume", "resume", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
-			new("Projects", "projects", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
-			new("Contact", "contact", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
-		];
+		new("Services", "services", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
+		new("Resume", "resume", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
+		new("Projects", "projects", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
+		new("Contact", "contact", NavigationItemCssType.Normal, NavigationItemCssActiveType.UnderLine),
+	];
 
 	private static readonly NavigationItem _contactNavigationLink = new("Let's Connect", "contact", NavigationItemCssType.Rounded, NavigationItemCssActiveType.Normal);
 	private static readonly NavigationItem _contactNavigationLinkAccent = new("Let's Connect", "contact", NavigationItemCssType.RoundedInverted, NavigationItemCssActiveType.Normal);
